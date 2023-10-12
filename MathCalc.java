e2083969dd2ddc37b2a05a176c5f9b9822e92114
aazin1368 <147614667+aazin1368@users.noreply.github.com>
2023-10-12 21:16:37 +0200

    Add files via upload

--- MathCalc.java
+++ MathCalc.java
@@ -1,22 +1,23 @@
-   public class MathCalc {
+public class MathCalc {
     public static void main(String[] args) {
-        // Given diameter of the Sun in miles
-        double diameter = 865000.0;
+        // Given diameter of the Earth in miles
+        double diameter = 7600.0;
 
-        // Calculate the radius
+        // Calculate the radius 
         double radius = diameter / 2.0;
 
         // Calculate the volume 
-        // Math.pow(x,y) is a function to calculate the value of x     raised to the power of y 
+// Math.pow(x,y) is a function to calculate the value of x     raised to the power of y 
+
         double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
 
    
 
-        // The volume of the Sun is Y cubic miles
-        double Y = volume;
+        // The volume of the Earth is X cubic miles
+        double X = volume;
 
         // Print the value of Y
-        System.out.println(" Y = " + Y);
+        System.out.println("X = " + X);
     }
 }
 
