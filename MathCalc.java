e8b44b039edfdceabdd68d04dd9a02746458197c
aazin1368 <147614667+aazin1368@users.noreply.github.com>
2023-10-12 21:14:33 +0200

    Add files via upload
    Task1 - question 1

---
+++ MathCalc.java
@@ -0,0 +1,24 @@
+   public class MathCalc {
+    public static void main(String[] args) {
+        // Given diameter of the Sun in miles
+        double diameter = 865000.0;
+
+        // Calculate the radius
+        double radius = diameter / 2.0;
+
+        // Calculate the volume 
+        // Math.pow(x,y) is a function to calculate the value of x     raised to the power of y 
+        double volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+
+   
+
+        // The volume of the Sun is Y cubic miles
+        double Y = volume;
+
+        // Print the value of Y
+        System.out.println(" Y = " + Y);
+    }
+}
+
+
+
